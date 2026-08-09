d8f83488304636a414b6416461973165f966cd7c
Jotta Kayo M Lima <jottakayoml@gmail.com>
2026-08-09 14:01:07 -0300

    Create clientset

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
 func main() {
+	home, _ := os.UserHomeDir()
+	kubeconfig := filepath.Join(home, ".kube", "config")
+
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Kubernetes client configuration loaded")
   
 }
