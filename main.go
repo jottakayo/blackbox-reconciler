30cfc3502631e48801ed4690b0977fea4c6ab5de
Jotta Kayo M Lima <jottakayoml@gmail.com>
2026-08-09 16:54:57 -0300

    filter ingress that there is annotation, its worked.

--- main.go
+++ main.go
@@ -4,25 +4,49 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"context"
 
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
 	home, _ := os.UserHomeDir()
-	kubeconfig := filepath.Join(home, ".kube", "config")
+	kubeconfig := filepath.Join(home, ".kube", "tools.yaml")
 
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		panic(err)
 	}
-
-	_, err = kubernetes.NewForConfig(config)
+	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
+	ctx := context.Background()
+	ingresses, err := clientset.NetworkingV1().Ingresses("sonarqube").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+    for _, ingress := range ingresses.Items {
+	if ingress.Spec.IngressClassName == nil {
+		continue
+	}
 
-	fmt.Println("Kubernetes client configuration loaded")
+	if *ingress.Spec.IngressClassName != "nginx" {
+		continue
+	}
+	enabled := ingress.Annotations["blackbox-reconciler.jottakayo.io/enabled"]
+
+	if enabled != "true" {
+		continue
+	}
+
+	for _, rule := range ingress.Spec.Rules {
+		fmt.Println(rule.Host)
+	}
+	}
+
+	//fmt.Printf("Kubernetes client created: %v\n", clientset != nil)
   
 }
