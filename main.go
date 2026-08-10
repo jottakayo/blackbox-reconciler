14e020650033977439b021edaa549d879e7118ea
Jotta Kayo M Lima <jottakayoml@gmail.com>
2026-08-10 17:51:38 -0300

    Viwer multi-clsuter

--- main.go
+++ main.go
@@ -11,6 +11,11 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+type MonitoringTarget struct {
+		Host string
+		Cluster string
+	}
+
 func main() {
 	home, _ := os.UserHomeDir()
 	kubeconfig := filepath.Join(home, ".kube", "desenvolvimento.yaml")
@@ -19,31 +24,45 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
+	rawConfig, err := clientcmd.LoadFromFile(kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	contextConfig := rawConfig.Contexts[rawConfig.CurrentContext]
+
+	for contextName, contextConfig := range rawConfig.Contexts {
+		fmt.Println("Context:", contextName)
+		fmt.Println("Cluster:", contextConfig.Cluster)
+	}
+
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
 	ctx := context.Background()
 
-	hosts, err := findMonitoredIngresses(clientset, ctx)
+	targets, err := findMonitoredIngresses(clientset, ctx, contextConfig.Cluster)
 	if err != nil {
 		panic(err)
 	}
 
-	for _, host := range hosts {
-		fmt.Println(host)
+	for _, target := range targets {
+		fmt.Println(target.Host, target.Cluster)
 	}
 
 }
-func findMonitoredIngresses(clientset *kubernetes.Clientset, ctx context.Context) ([]string, error) {
+func findMonitoredIngresses(clientset *kubernetes.Clientset, ctx context.Context,
+	cluster string) ([]MonitoringTarget, error) {
 	ingresses, err := clientset.NetworkingV1().Ingresses(metav1.NamespaceAll).List(ctx, metav1.ListOptions{})
 	if err != nil {
 		return nil, err
 	}
 
-
-	var hosts []string
+	var targets []MonitoringTarget
 	
+    
 
 	for _, ingress := range ingresses.Items {
 		if ingress.Spec.IngressClassName == nil {
@@ -59,9 +78,12 @@ func findMonitoredIngresses(clientset *kubernetes.Clientset, ctx context.Context
 		}
 
 		for _, rule := range ingress.Spec.Rules {
-			hosts = append(hosts, rule.Host)
+			targets = append(targets, MonitoringTarget{
+				Cluster: cluster,
+				Host: rule.Host,
+			})
 		}
 	}
 
-	return hosts, nil
+	return targets, nil
 }
